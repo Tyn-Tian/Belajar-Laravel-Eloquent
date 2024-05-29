c443c1c06cf9e1216fa9edef843e108a77c73a8e
Christian <kristian2015ok@gmail.com>
2024-05-29 21:06:31 +0700

    Add: Many To Many

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Customer;
+use App\Models\Product;
 use App\Models\VirtualAccount;
 use App\Models\Wallet;
+use Database\Seeders\CategorySeeder;
 use Database\Seeders\CustomerSeeder;
+use Database\Seeders\ProductSeeder;
 use Database\Seeders\VirtualAccountSeeder;
 use Database\Seeders\WalletSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -57,5 +61,30 @@ class CustomerTest extends TestCase
         $virtualAccount = $customer->virtualAccount;
         self::assertNotNull($virtualAccount);
         self::assertEquals("BCA", $virtualAccount->bank);
+    }
+
+    public function testManyToMany()
+    {
+        $this->seed([CustomerSeeder::class, CategorySeeder::class, ProductSeeder::class]);
+
+        $customer = Customer::find("TYN");
+        self::assertNotNull($customer);
+        
+        $customer->likeProducts()->attach("1");
+        $products = $customer->likeProducts;
+        self::assertCount(1, $products);
+
+        self::assertEquals("1", $products[0]->id);
+    }
+
+    public function testManyToManyDetach()
+    {
+        $this->testManyToMany();
+
+        $customer = Customer::find("TYN");
+        $customer->likeProducts()->detach("1");
+
+        $products = $customer->likeProducts;
+        self::assertCount(0, $products);
     }
 }
