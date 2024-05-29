e9b21a65a863b5dc5e1726908f11d667ffd634f5
Christian <kristian2015ok@gmail.com>
2024-05-29 14:20:51 +0700

    Add: One to Many

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testOneToMany()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $product = Product::find("1");
+        self::assertNotNull($product);
+
+        $category = $product->category;
+        self::assertNotNull($category);
+        self::assertEquals("FOOD", $category->id);
+    }
+}
