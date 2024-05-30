8c273a06f829373f59c5997500e68e69f7c6e294
Christian <kristian2015ok@gmail.com>
2024-05-30 12:51:46 +0700

    Add: Attribute Casting

--- tests/Feature/PersonTest.php
+++ tests/Feature/PersonTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Person;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -28,5 +29,18 @@ class PersonTest extends TestCase
 
         self::assertEquals("JOKO", $person->first_name);
         self::assertEquals("Morro", $person->last_name);
+    }
+
+    public function testAttributeCasting()
+    {
+        $person = new Person();
+        $person->first_name = "Chris";
+        $person->last_name = "Tian";
+        $person->save();
+
+        self::assertNotNull($person->created_at);
+        self::assertNotNull($person->updated_at);
+        self::assertInstanceOf(Carbon::class, $person->created_at);
+        self::assertInstanceOf(Carbon::class, $person->updated_at);
     }
 }
