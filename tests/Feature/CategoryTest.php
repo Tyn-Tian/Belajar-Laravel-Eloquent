6ccb022242df80d51c5cd8717d61a55ff93629bd
Christian <kristian2015ok@gmail.com>
2024-05-30 12:11:56 +0700

    Add: Aggregating Relations

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -268,4 +268,16 @@ class CategoryTest extends TestCase
         self::assertCount(1, $products);
         self::assertEquals("2", $products[0]->id);
     }
+
+    public function testAggregatingRelations()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::find("FOOD");
+        $products = $category->products()->count();
+        self::assertEquals(2, $products);
+
+        $products = $category->products()->where('price', 200)->count();
+        self::assertEquals(1, $products);
+    }
 }
